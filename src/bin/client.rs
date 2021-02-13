635d79b6f68ea628fca053bfb701be01aeaf63ed
Sebastian Geisler <sebastian@blockstream.io>
2021-02-13 14:23:36 +0100

    implement client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,13 +1,49 @@
-use structopt::StructOpt;
+use btcbc::{Network, Request, Transaction};
+use futures::SinkExt;
 use nym_addressing::clients::Recipient;
-use btcbc::Network;
+use structopt::StructOpt;
+use tokio_tungstenite::connect_async;
+use tokio_tungstenite::tungstenite::Message;
 
 #[derive(StructOpt)]
 struct Options {
+    #[structopt(short, long, default_value = "ws://127.0.0.1:1977")]
+    websocket: String,
     #[structopt(parse(try_from_str = Recipient::try_from_base58_string))]
     service_provider: Recipient,
+    #[structopt(short, long, default_value = "bitcoin")]
     network: Network,
-    transaction: String,
+    transaction: Transaction,
 }
 
-fn main() {}
+impl Options {
+    fn into_parts(self) -> (String, Request, Recipient) {
+        let req = Request {
+            network: self.network,
+            transaction: self.transaction,
+        };
+        (self.websocket, req, self.service_provider)
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let opts: Options = StructOpt::from_args();
+    let (websocket, request, recipient) = opts.into_parts();
+
+    let (mut ws, _) = connect_async(&websocket)
+        .await
+        .expect("Couldn't connect to nym websocket");
+
+    let nym_packet = nym_websocket::requests::ClientRequest::Send {
+        recipient,
+        message: bincode::serialize(&request).expect("can't fail"),
+        with_reply_surb: false,
+    };
+
+    ws.send(Message::Binary(nym_packet.serialize()))
+        .await
+        .expect("couldn't send request");
+
+    ws.close(None).await.expect("Failed to close websocket.");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-use serde::{Serialize, Deserialize};
-use std::str::FromStr;
-use std::fmt::{Display, Formatter};
+use serde::{Deserialize, Serialize};
 use std::error::Error;
+use std::fmt::{Display, Formatter};
+use std::str::FromStr;
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Serialize, Deserialize)]
 pub enum Network {
@@ -9,6 +9,15 @@ pub enum Network {
     Testnet,
     Liquid,
 }
+
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Request {
+    pub network: Network,
+    pub transaction: Transaction,
+}
+
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Transaction(pub Vec<u8>);
 
 impl Network {
     pub fn api_endpoint(&self) -> &str {
@@ -40,7 +49,22 @@ impl FromStr for Network {
             "testnet" => Ok(Network::Testnet),
             "liquid" => Ok(Network::Liquid),
             // Annoying that we need to clone here, but `FromStr::Err` doesn't allow lifetimes
-            other => Err(UnknownNetworkError(other.to_string()))
+            other => Err(UnknownNetworkError(other.to_string())),
         }
+    }
+}
+
+impl FromStr for Transaction {
+    type Err = hex::FromHexError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Transaction(hex::decode(s)?))
+    }
+}
+
+impl Display for Transaction {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        // Why no `encode_to_fmt`???
+        f.write_str(&hex::encode(&self.0))
     }
 }
